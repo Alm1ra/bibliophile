74bfe7f83ddb0e70914b67e1b5f2b329737cb0be
Alm1ra <frydlewicz.ola@gmail.com>
2023-09-17 01:31:59 +0200

    Added a hover functionality to the sidebar

--- bibliophile/src/components/Sidebar.js
+++ bibliophile/src/components/Sidebar.js
@@ -6,18 +6,34 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { faChartSimple } from '@fortawesome/free-solid-svg-icons';
 import './Sidebar.css';
 export default function Sidebar() {
+
+var isCollapsed = false;
+
+  function menuBtnHandler() {
+    const bar = document.querySelector('.bar');
+
+    if(!isCollapsed) {
+      bar.classList.add('hover-collapse');
+      isCollapsed = true;
+    }
+    else {
+      bar.classList.remove('hover-collapse');
+      isCollapsed = false;
+    }
+  }
+
     return (
         <div className="bar">
-            <div id="menu">
-              <h3>MENU</h3>
-              <div id="menu-btn"><FontAwesomeIcon icon={faBarsStaggered} /></div>
+            <div className="menu">
+              <h3><span className='bar-text'>MENU</span></h3>
+              <button onClick={menuBtnHandler} id="menu-btn"><FontAwesomeIcon icon={faBarsStaggered} /></button>
             </div>
             <div className="navbar">
               <ul>
-                <a href="./"><li><FontAwesomeIcon className='icon' icon={faHouse} /> HOME</li></a>
-                <a href="#"><li><FontAwesomeIcon className='icon' icon={faPlus} /> ADD</li></a>
-                <a href="#"><li><FontAwesomeIcon className='icon' icon={faMagnifyingGlass} /> SEARCH</li></a>
-                <a href="#"><li><FontAwesomeIcon className='icon' icon={faChartSimple} /> STATISTICS</li></a>
+                <a href="./"><li><FontAwesomeIcon className='icon' icon={faHouse}/><span className='bar-text'> HOME</span></li></a>
+                <a href="#"><li><FontAwesomeIcon className='icon' icon={faPlus} /><span className='bar-text'> ADD</span></li></a>
+                <a href="#"><li><FontAwesomeIcon className='icon' icon={faMagnifyingGlass} /><span className='bar-text'> SEARCH</span></li></a>
+                <a href="#"><li><FontAwesomeIcon className='icon' icon={faChartSimple} /><span className='bar-text'> STATISTICS</span></li></a>
               </ul>
             </div>
         </div>
