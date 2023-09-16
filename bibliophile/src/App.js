27676f65c5a26839efded3fcf37ddd86e3e553fa
Alm1ra <frydlewicz.ola@gmail.com>
2023-09-16 15:06:49 +0200

    Uploaded code from an old project

--- bibliophile/src/App.js
+++ bibliophile/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="home">
+        <div class="content">
+          <div class="bar">
+            <div id="menu">
+              <h3>MENU</h3>
+              <div id="menu-btn"><i class="fa-solid fa-bars-staggered"></i></div>
+            </div>
+            <div class="navbar">
+              <ul>
+                <a href="#"><li><i class="fa-solid fa-house"></i> HOME</li></a>
+                <a href="#"><li><i class="fa-solid fa-plus"></i> ADD</li></a>
+                <a href="#"><li><i class="fa-solid fa-magnifying-glass"></i> SEARCH</li></a>
+                <a href="#"><li><i class="fa-solid fa-chart-simple"></i> STATISTIC</li></a>
+              </ul>
+            </div>
+          </div>
+          
+          <div class="header">
+            <div class="desc">
+            <h1>BIBLIOPHILE</h1>
+            <h2>Dive into a world of books</h2>
+            <h2>Discover new reads</h2>
+            <h2>Your next adventure begins now</h2>
+              <form action="#">
+                <input type="submit" value="SEARCH NOW"/>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="footer">
+        <h4>© 2023 Copyright: Aleksandra Frydlewicz</h4>
+        </div>
+      </div>
     </div>
   );
 }
