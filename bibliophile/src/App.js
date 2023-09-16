4a463c8cd30e91fa56ee01dd7506021bce729109
Alm1ra <frydlewicz.ola@gmail.com>
2023-09-16 16:38:19 +0200

    Created a component for the sidebar

--- bibliophile/src/App.js
+++ bibliophile/src/App.js
@@ -1,33 +1,14 @@
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBarsStaggered } from '@fortawesome/free-solid-svg-icons';
-import { faHouse } from '@fortawesome/free-solid-svg-icons';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { faChartSimple } from '@fortawesome/free-solid-svg-icons';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <div class="home">
-        <div class="content">
-          <div class="bar">
-            <div id="menu">
-              <h3>MENU</h3>
-              <div id="menu-btn"><FontAwesomeIcon icon={faBarsStaggered} /></div>
-            </div>
-            <div class="navbar">
-              <ul>
-                <a href="./"><li><FontAwesomeIcon icon={faHouse} /> HOME</li></a>
-                <a href="#"><li><FontAwesomeIcon icon={faPlus} /> ADD</li></a>
-                <a href="#"><li><FontAwesomeIcon icon={faMagnifyingGlass} /> SEARCH</li></a>
-                <a href="#"><li><FontAwesomeIcon icon={faChartSimple} /> STATISTIC</li></a>
-              </ul>
-            </div>
-          </div>
-          
-          <div class="header">
-            <div class="desc">
+      <div className="home">
+        <div className="content">
+          <Sidebar/>         
+          <div className="header">
+            <div className="desc">
             <h1>BIBLIOPHILE</h1>
             <h2>Dive into a world of books</h2>
             <h2>Discover new reads</h2>
@@ -38,7 +19,7 @@ function App() {
             </div>
           </div>
         </div>
-        <div class="footer">
+        <div className="footer">
         <h4>© 2023 Copyright: Aleksandra Frydlewicz</h4>
         </div>
       </div>
---
+++ bibliophile/src/components/Sidebar.js
@@ -0,0 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBarsStaggered } from '@fortawesome/free-solid-svg-icons';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faChartSimple } from '@fortawesome/free-solid-svg-icons';
+import './Sidebar.css';
+export default function Sidebar() {
+    return (
+        <div className="bar">
+            <div id="menu">
+              <h3>MENU</h3>
+              <div id="menu-btn"><FontAwesomeIcon icon={faBarsStaggered} /></div>
+            </div>
+            <div className="navbar">
+              <ul>
+                <a href="./"><li><FontAwesomeIcon icon={faHouse} /> HOME</li></a>
+                <a href="#"><li><FontAwesomeIcon icon={faPlus} /> ADD</li></a>
+                <a href="#"><li><FontAwesomeIcon icon={faMagnifyingGlass} /> SEARCH</li></a>
+                <a href="#"><li><FontAwesomeIcon icon={faChartSimple} /> STATISTIC</li></a>
+              </ul>
+            </div>
+        </div>
+    );
+}
