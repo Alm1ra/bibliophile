bed3775a79f4be33addab38a6e3c29cfc685f708
Alm1ra <frydlewicz.ola@gmail.com>
2023-09-17 22:04:26 +0200

    Added a new subpage

--- bibliophile/src/pages/AddPage.js
+++ bibliophile/src/pages/AddPage.js
@@ -1,3 +1,51 @@
+import './AddPage.css'
+
 export default function AddPage() {
-    
+    return (
+        <div className='add-content'>
+            <div className='add_form'>
+                <h1>ADD YOUR BOOK</h1>
+                <form action="#">
+                    <label form="title">Title:</label>
+                    <input type="text" id="title" name="title" value=""></input>
+
+                    <label form="author">Author:</label>
+                    <input type="text" id="author" name="author" value=""></input>
+
+                    <label form="genre">Genre:</label>
+                    <select id="genre" name="genre">
+                    <option value="adventure">Adventure</option>
+                        <option value="art">Art</option>
+                        <option value="children’s">Children’s</option>
+                        <option value="contemporary">Contemporary</option>
+                        <option value="cookbook">Cookbook</option>
+                        <option value="development">Development</option>
+                        <option value="dystopian">Dystopian</option>
+                        <option value="families_relationships">Families & Relationships</option>
+                        <option value="fantasy">Fantasy</option>
+                        <option value="guide">Guide</option>
+                        <option value="health">Health</option>
+                        <option value="historical_fiction">Historical fiction</option>
+                        <option value="history">History</option>
+                        <option value="horror">Horror</option>
+                        <option value="humor">Humor</option>
+                        <option value="memoir">Memoir</option>
+                        <option value="motivational">Motivational</option>
+                        <option value="mystery">Mystery</option>
+                        <option value="paranormal">Paranormal</option>
+                        <option value="romance">Romance</option>
+                        <option value="science_fiction">Science Fiction</option>
+                        <option value="self_help">Self-help</option>
+                        <option value="thriller">Thriller</option>
+                        <option value="travel">Travel</option>
+                    </select>
+
+                    <label form="summ">Summary:</label>
+                    <textarea id="text" name="summ" rows="5" cols="46"></textarea>
+                
+                    <input type="submit" className='add-input' value="SUBMIT"></input>
+                </form>
+            </div>
+        </div>
+    );
 }
--- bibliophile/src/pages/HomePage.js
+++ bibliophile/src/pages/HomePage.js
@@ -2,14 +2,14 @@ import './HomePage.css'
 
 export default function HomePage() {
     return (
-        <div className="content">
+        <div className="home-content">
             <div className="desc">
                 <h1>BIBLIOPHILE</h1>
                 <h2>Dive into a world of books</h2>
                 <h2>Discover new reads</h2>
                 <h2>Your next adventure begins now</h2>
                 <form action="#">
-                    <input type="submit" value="SEARCH NOW"/>
+                    <input type="submit" className='home-input' value="SEARCH NOW"/>
                 </form>
             </div>
         </div>
