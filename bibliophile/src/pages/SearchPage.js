166e783d414f749b990c2422d9d0685ee2e26812
Alm1ra <frydlewicz.ola@gmail.com>
2023-09-18 13:41:28 +0200

    Added a "search" subpage

--- bibliophile/src/pages/SearchPage.js
+++ bibliophile/src/pages/SearchPage.js
@@ -1,3 +1,18 @@
+import './SearchPage.css';
+
 export default function SearchPage() {
-    
+    return (
+        <div className='search-content'>
+            <div className='search-header'>
+                <h1>SEARCH FOR A BOOK</h1>
+                <form className='search-form' action='#'>
+                    <input type="text" id="search" name="search" value=""></input>
+                    <input type="submit" className='search-button' value="SEARCH"></input>  
+                </form>
+            </div>
+            <div className='search-result'>
+                Book1
+            </div>
+        </div>
+    );
 }
