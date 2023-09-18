0a388635a677c599ca9d9837bfd164f72f8d8589
Alm1ra <frydlewicz.ola@gmail.com>
2023-09-18 19:44:57 +0200

    Fixed responsiveness in the "search" subpage

--- bibliophile/src/pages/SearchPage.js
+++ bibliophile/src/pages/SearchPage.js
@@ -4,11 +4,13 @@ export default function SearchPage() {
     return (
         <div className='search-content'>
             <div className='search-header'>
-                <h1>SEARCH FOR A BOOK</h1>
-                <form className='search-form' action='#'>
-                    <input type="text" id="search" name="search" value=""></input>
-                    <input type="submit" className='search-button' value="SEARCH"></input>  
-                </form>
+                <div className='search-header-cont'>
+                    <h1>SEARCH FOR A BOOK</h1>
+                    <form className='search-form' action='#'>
+                        <input type="text" className="search-text" name="search" value=""></input>
+                        <input type="submit" className='search-button' value="SEARCH"></input>  
+                    </form>
+                </div>              
             </div>
             <div className='search-result'>
                 Book1
